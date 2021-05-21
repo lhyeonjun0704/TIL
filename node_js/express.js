2f3d17ceeaa53b803ada18b429be83ceca64bf03
Hyonjun Lee <hello.git.toqur7000@naver.com>
2021-05-21 16:47:22 +0900

    node_js

--- node_js/express.js
+++ node_js/express.js
@@ -1,5 +1,50 @@
 const express = require('express');
+const hbs = require('express-handlebars'); //handle-bars
+const bodyParser = require('body-parser');
+
 const server = express();
+const words = require('./db/words.json');
+
+
+
+server.engine( 
+    "hbs",
+    hbs({
+    extname:'hbs',
+    defaultLayout:"layout.hbs",
+    partialsDir:"partials", //부분적인 html파일을 넣을 때 사용한다.
+    })
+);
+
+server.set('view engine','hbs');
+server.use(express.static(__dirname + '/public')); // 이제 파일 가져오는거 public부터 시작
+server.use(bodyParser.urlencoded({extended:false}));
+
+server.get('/', (req,res) => {
+    res.render('home', {
+        words,
+    });
+});
+
+server.post('/', (req,res) =>{
+    const {query} = req.body;
+    res.render('home',{
+        words:words.filter(w=>w.word.toLocaleLowerCase().includes(query.toLocaleLowerCase())),
+    });
+});
+
+server.delete('/', (req,res) => {
+    let {word} = req.body;
+    words = words.filter(w=>!(w.word === word));
+});
+
+server.get('/add', (req,res) => {
+    res.render('add');
+});
+
+server.get('/quiz', (req,res) => {
+    res.render('quiz');
+});
 
 // server.get('/', (req,res) =>{
 //     res.send('<h1>hello LHJ</h1>');
@@ -10,23 +55,27 @@ const server = express();
 // PUT      
 
 // 모든 서버의 요청들이 이 use를 통해서 지나가야된다.
-server.use((req, res, ) => {
-    req.user = {
-        id: '1234',
-    };
-    next();
-});
+// server.use((req, res, next) => {
+//     req.user = {
+//         id: '1234',
+//     };
+//     next();
+// });
 
-server.get('/', (req, res) => {
-    //__dirname // 자신의 파일 경로를 찾고 싶을 때 쓰면 됨.
-    //__filename // 현재 실행되는 파일의 이름을 얻을 수 있음.
-    res.sendFile(__dirname + '/test.html');
-});
+// server.get('/', (req, res) => {
+//     //__dirname // 자신의 파일 경로를 찾고 싶을 때 쓰면 됨.
+//     //__filename // 현재 실행되는 파일의 이름을 얻을 수 있음.
+//     console.log(req.user);
+//     res.sendFile(__dirname + '/test.html');
+// });
 
-server.get('/about', (req, res) => {
-    res.sendFile(__dirname + '/about.html');
-});
+// server.get('/about', (req, res) => {
+//     res.sendFile(__dirname + '/about.html');
+// });
 
+// server.use((req, res) => {
+//     res.sendFile(__dirname + '/404.html');
+// }); 
 
 server.listen(3001, (err) =>{
     if(err) return console.log(err);
