b83373ad2de1bc5a6a20ca508d236effed7fb7b8
Hyonjun Lee <hello.git.toqur7000@naver.com>
2021-06-10 14:34:19 +0900

    jquey_part2_end

--- javascript/new/tab.js
+++ javascript/new/tab.js
@@ -19,11 +19,30 @@
 //     $('.tab-content').eq(2).addClass('show');
 // });
 
-for (let i = 0; i < $('.tab-button').length; i++){
-    $('.tab-button').eq(i).click(function(){
-        $('.tab-button').removeClass('active');
-        $('.tab-button').eq(i).addClass('active');
-        $('.tab-content').removeClass('show');
-        $('.tab-content').eq(i).addClass('show');
-    });
+// for (let i = 0; i < $('.tab-button').length; i++){
+//     $('.tab-button').eq(i).click(function(){
+//         $('.tab-button').removeClass('active');
+//         $('.tab-button').eq(i).addClass('active');
+//         $('.tab-content').removeClass('show');
+//         $('.tab-content').eq(i).addClass('show');
+//     });
+// }
+
+
+// 이런식으로 값을 저장도 가능
+// $('.list').data('name', 'value');
+// $('.list').data('name');
+
+
+
+$('.list').click(function(e){
+    
+        opentab(e.target.dataset.id);
+});
+
+function opentab(n){
+    $('.tab-button').removeClass('active');
+    $('.tab-button').eq(n).addClass('active');
+    $('.tab-content').removeClass('show');
+    $('.tab-content').eq(n).addClass('show');
 }
